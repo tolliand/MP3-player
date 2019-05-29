bd1064742758767ff66035f4dd4f85048499bba7
tolliand <maria.selivanovaa@yandex.ru>
2019-05-30 00:32:46 +0300

    main activity is working!

--- app/src/main/java/com/example/mp3player/MainActivity.kt
+++ app/src/main/java/com/example/mp3player/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.example.mp3player
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+/*
+    private lateinit var playerView: PlayerView
+    private lateinit var player: SimpleExoPlayer
+*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+ /*       player = ExoPlayerFactory.newSimpleInstance(this)
+        playerView = findViewById(R.id.playerView)
+        playerView.player = player
+
+        // Produces DataSource instances through which media data is loaded.
+        val dataSourceFactory = DefaultDataSourceFactory(
+            this,
+            getUserAgent(this, "MP3 player")
+        )
+
+       val audioSource =*/
+// This is the MediaSource representing the media to be played.
+        /*val audioSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(mp4VideoUri)
+        player.prepare(audioSource)*/
     }
+
+    fun startSearch(view: View?) {
+        val intent = Intent(this@MainActivity, TrackList::class.java)
+        startActivity(intent)
+    }
+/*
+    override fun onStop() {
+        super.onStop()
+
+        playerView.player = null
+        player.release()
+        //player = null
+    }
+    */
 }
---
+++ app/src/main/java/com/example/mp3player/TrackList.kt
@@ -0,0 +1,12 @@
+package com.example.mp3player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrackList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_track_list)
+    }
+}
